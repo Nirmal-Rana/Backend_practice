6c682f79998e77e3f35c4ba293f64dd4e14fc481
Nirmal-Rana <nirmalra43@gmail.com>
2026-02-13 14:17:33 +0545

    express completed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello, World!');
+const express=require('express');
+const app=express();
+
+const port=5000;
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`);
+});
+
+app.get("/",(req,res)=>{
+    res.send("Nirmal Rana Mager")
+});
+console.log("helo wold");
